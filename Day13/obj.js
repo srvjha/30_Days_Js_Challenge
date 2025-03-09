be98f6db528aa9fe594518efdb606653dd2e4dbf
Saurav Jha <94675822+srvjha@users.noreply.github.com>
2025-03-09 23:35:05 +0530

    Update obj.js

--- Day13/obj.js
+++ Day13/obj.js
@@ -11,7 +11,7 @@ export const person = {
      hobbies: ['Cricket', 'Football', 'Gaming'],
 
      bio(){
-        return `My name is ${this.name} and I am ${this.age} years old
+        return `My name is ${this.name} and I am ${this.age}
             and I live in ${this.address.city}, ${this.address.state}, ${this.address.country}.
             My hobbies are ${this.hobbies[0]}, ${this.hobbies[1]} and ${
             this.hobbies[2]}.`
